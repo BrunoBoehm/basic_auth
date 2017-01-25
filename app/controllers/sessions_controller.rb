5e1238f7bdb0600450b75929899e8e95347d42d4
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-25 16:13:07 +0100

    added a logout link and current_user logged_in helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		# raise cookies.inspect
+		user = User.find_by(email: params[:email])
+		session[:user_id] = user.id
+		redirect_to user_path(user)
+	end
+
+	def destroy
+		reset_session
+		redirect_to new_session_path
+	end
 end
