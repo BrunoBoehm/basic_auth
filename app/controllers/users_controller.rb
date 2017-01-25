eb24215dea6313d716f9fa3126130a43a5b5d9d6
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-25 15:34:08 +0100

    generated sessions controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
-	def new
-		@user = User.new
+# GET /users/new
+def new
+	@user = User.new
+end
+
+# POST /users
+def create
+	# raise params.inspect
+	# "user"=>{"username"=>"test", "email"=>"test@gmail.com", "password"=>"test"}
+	user = User.new(user_params)
+	if user.save
+		redirect_to user
+	else
+		render :new
+	end
+end
+
+# GET users/:id
+def show
+	@user = User.find_by(id: params[:id])
+end
+
+private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
